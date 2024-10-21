14a342eb7c7263f888c1b69fe59aa779964c0add
Danilo Jesús <danilojesus112001@gmail.com>
2024-10-21 15:52:46 -0300

    Adicionando o button calcular

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,14 @@
 const inputConta = document.querySelector('.js-conta-input')
 const contaValor = inputConta.value
+console.log(typeof contaValor)
 
 const calculaTaxa = (value) => {
-  contaValor * (value || 23)
+  const valorTaxa = document.querySelector('.js-texto-taxa')
+  const valorPagamento = document.querySelector('.js-texto-pagamento')
+
+  const pagamento = parseFloat(contaValor) * (value / 100)
+
+  console.log(pagamento)
+  
 }
+
